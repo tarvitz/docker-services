e7fdd61f404411aab0b151b1ac820430afdd8587
Tarvitz <tarvitz@blacklibrary.ru>
2016-11-20 01:17:47 +0300

    [nginx] update master register script and example

--- nginx/register.sh
+++ nginx/register.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker run -d --name nginx-master --restart always \
+
+docker run -d --name nginx-master --restart always --network bridge\
+           --add-host="master:172.17.0.1"\
            -v ~/www/docker/nginx:/etc/nginx/conf.d -p 80:80 -p 443:443 nfox/nginx
